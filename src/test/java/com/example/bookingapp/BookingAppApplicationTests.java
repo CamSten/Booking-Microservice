f1399683a2cb462d8b4199c0ab9bd7c30859e2bb
CamSten <camilla.stenklov@gmail.com>
2026-09-18 12:27:20 +0200

    Adding MockBean for restTemplate.

--- src/test/java/com/example/bookingapp/BookingAppApplicationTests.java
+++ src/test/java/com/example/bookingapp/BookingAppApplicationTests.java
@@ -1,16 +1,23 @@
 package com.example.bookingapp;
 
+import com.example.bookingapp.config.RestTemplateConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 import java.util.Collections;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,7 +37,20 @@ class BookingAppApplicationTests {
             .withDatabaseName("bookify")
             .withUsername("test")
             .withPassword("test");
+    @MockBean(answer = Answers.RETURNS_DEEP_STUBS)
+    private RestTemplateConfig restTemplateConfig;
+    @BeforeEach
+    void mockCustomerService() {
+        when(restTemplateConfig.restTemplate().getForEntity(
+                "http://localhost:8081/api/customers/1",
+                Object.class))
+                .thenReturn(ResponseEntity.ok().build());
 
+        when(restTemplateConfig.restTemplate().getForEntity(
+                "http://localhost:8081/api/customers/-1",
+                Object.class))
+                .thenReturn(ResponseEntity.notFound().build());
+    }
     @Autowired MockMvc mvc;
     private final String bookingJson = """
                 {
